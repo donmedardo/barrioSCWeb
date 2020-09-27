16a0c5030563f9dde6f316b7910158462197a66d
GermanSimba <germansimbasan@gmail.com>
2020-09-27 16:10:25 -0500

    cambio idex

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 //servidor de node para angular
 const app = express();
 
-app.use(express.static('./dist/developers'));
+app.use(express.static('./dist/barrioSC'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/developers/' }),
+    res.sendFile('index.html', { root: 'dist/barrioSC/' }),
 );
 
 app.listen(process.env.PORT || 8080);
